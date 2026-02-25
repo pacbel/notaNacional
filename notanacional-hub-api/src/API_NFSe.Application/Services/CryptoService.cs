3308f934a26ca6eda8612fc8afadc77a34c81c43
pacbel <carlos.pacheco@pacbel.com.br>
2026-02-25 18:46:35 -0300

    Ajuste criptografia

--- notanacional-hub-api/src/API_NFSe.Application/Services/CryptoService.cs
+++ notanacional-hub-api/src/API_NFSe.Application/Services/CryptoService.cs
@@ -14,21 +14,28 @@ public class CryptoService : ICryptoService
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
     }
 
+    // Bytes indefinidos no Windows-1252 que o VB6 Chr$() ignora
+    private static readonly HashSet<byte> Win1252Undefined = new() { 0x81, 0x8D, 0x8F, 0x90, 0x9D };
+
     public byte[] Encrypt(string text)
     {
         var encoding = Encoding.GetEncoding(1252);
         var chaveBytes = encoding.GetBytes(Chave);
         var textBytes = encoding.GetBytes(text);
-        var result = new byte[textBytes.Length];
+        var result = new List<byte>();
 
         int indChave = 0;
         for (int i = 0; i < textBytes.Length; i++)
         {
-            result[i] = (byte)(textBytes[i] + chaveBytes[indChave]);
+            var charTmp = (byte)(textBytes[i] + chaveBytes[indChave]);
+
+            if (!Win1252Undefined.Contains(charTmp))
+                result.Add(charTmp);
+
             indChave = (indChave + 1) % chaveBytes.Length;
         }
 
-        return result;
+        return result.ToArray();
     }
 
     public string Decrypt(byte[] bytes)
